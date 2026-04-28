8df2b2a14c6258f3fde30053d398d2ca01126300
taniabhat <taniabhat2017@gmail.com>
2026-04-28 17:16:30 +0530

    fix

--- Travel/Travel-Backend/src/routes/tourRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTours, getTour, createTour } = require('../controllers/tourController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.route('/').get(getTours).post(protect, admin, createTour);
-router.route('/:id').get(getTour);
-
-module.exports = router;
